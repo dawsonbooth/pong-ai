6cb6c9c52502455db407541f1e86cd6a2d2cf25b
Dawson Booth <github@dawsonbooth.com>
2018-08-23 17:55:43 -0500

    Cleaned up directory

--- pong.py
+++ pong.py
@@ -5,15 +5,17 @@ from game import Game
 
 # Dictionary to associate argument with agent
 agents_dict = {
-    "player":Player,
-    "basic":AI.Basic,
-    "q-learning":AI.QLearning,
+    "player": Player,
+    "basic": AI.Basic,
+    "q-learning": AI.QLearning,
 }
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-a1", "--agent1", type=str, choices=["player", "basic", "q-learning",], help="Type of player for the left paddle")
-parser.add_argument("-a2", "--agent2", type=str, choices=["player", "basic", "q-learning",], help="Type of player for the right paddle")
+parser.add_argument("-a1", "--agent1", type=str, choices=[
+                    "player", "basic", "q-learning", ], help="Type of player for the left paddle")
+parser.add_argument("-a2", "--agent2", type=str, choices=[
+                    "player", "basic", "q-learning", ], help="Type of player for the right paddle")
 args = parser.parse_args()
 
 # Main function
@@ -21,7 +23,7 @@ def main():
     # Set up game
     pong = Game()
     pong.setup()
-    
+
     # Agent 1 - left paddle
     if args.agent1:
         p1 = agents_dict[args.agent1](pong, pong.paddle1)
@@ -33,12 +35,13 @@ def main():
         p2 = agents_dict[args.agent2](pong, pong.paddle2)
     else:
         p2 = AI.QLearning(pong, pong.paddle2)
-    
+
     # Game loop
-    while True: 
+    while True:
         pong.advance()
         p1.act()
         p2.act()
-        
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
