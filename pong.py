847263a68a7112baafc3b4be5b9c187428b55658
Dawson Booth <github@dawsonbooth.com>
2018-09-05 01:39:04 -0500

    Updated argument parsing

--- pong.py
+++ pong.py
@@ -9,17 +9,18 @@ def main(args):
     pong = Game()
     pong.setup()
 
+    # Dictionary to associate argument with agent
+    agents_dict = {
+        'player': Player,
+        'basic': AI.Basic,
+        'q-learning': AI.QLearning,
+    }
+
     # Agent 1 - left paddle
-    if args.agent1:
-        p1 = agents_dict[args.agent1](pong, pong.paddle1)
-    else:
-        p1 = AI.Basic(pong, pong.paddle1)
+    p1 = agents_dict[args.agent1](pong, pong.paddle1)
 
     # Agent 2 - right paddle
-    if args.agent2:
-        p2 = agents_dict[args.agent2](pong, pong.paddle2)
-    else:
-        p2 = AI.QLearning(pong, pong.paddle2)
+    p2 = agents_dict[args.agent2](pong, pong.paddle2)
 
     # Game loop
     while True:
@@ -29,19 +30,13 @@ def main(args):
 
 
 if __name__ == '__main__':
-    # Dictionary to associate argument with agent
-    agents_dict = {
-        "player": Player,
-        "basic": AI.Basic,
-        "q-learning": AI.QLearning,
-    }
-
     # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a1", "--agent1", type=str, choices=[
-                        "player", "basic", "q-learning", ], help="Type of player for the left paddle")
-    parser.add_argument("-a2", "--agent2", type=str, choices=[
-                        "player", "basic", "q-learning", ], help="Type of player for the right paddle")
+    parser = argparse.ArgumentParser(
+        description='Play pong vs your favorite form of AI')
+    parser.add_argument('-a1', '--agent1', nargs='?', type=str, choices=[
+                        'player', 'basic', 'q-learning', ], default='basic', help='Type of player for the left paddle')
+    parser.add_argument('-a2', '--agent2', nargs='?', type=str, choices=[
+                        'player', 'basic', 'q-learning', ], default='q-learning', help='Type of player for the right paddle')
     args = parser.parse_args()
 
     # Run program
